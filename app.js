6128616f186474a4d59f3961382cfcfbfd4fffa7
ManuelHMR <manuelhmrjf@gmail.com>
2022-04-29 17:24:46 -0300

    db setup & some modules configs

--- app.js
+++ app.js
@@ -1,5 +1,34 @@
 import express from "express";
+import cors from "cors";
+import {MongoClient} from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
-
 app.listen(5000);
+app.use(express.json());
+app.use(cors());
+
+const mongoClient = new MongoClient("mongodb://localhost:27017");
+let db;
+mongoClient.connect().then(() => {
+    db = mongoClient.db("db-batepapo-uol");
+})
+
+let user;
+let participants = [];
+let mensages = [];
+
+// app.post("/participants", async (req, res) => {
+//     try{
+//         wait 
+//     }
+// });
+
+// app.get("/participants");
+
+// app.post("/messages");
+
+// app.get("/messages");
+
+// app.post("/status");
