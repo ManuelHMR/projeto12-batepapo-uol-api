a80fe27f7e8c26caf962ca8d08cdd59266377ba2
ManuelHMR <manuelhmrjf@gmail.com>
2022-04-30 17:59:10 -0300

    dotenv & db connection & user post

--- app.js
+++ app.js
@@ -1,29 +1,42 @@
 import express from "express";
 import cors from "cors";
-import {MongoClient} from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
 import dotenv from "dotenv";
-dotenv.config();
+import joi from "joi";
 
 const app = express();
 app.listen(5000);
 app.use(express.json());
 app.use(cors());
+dotenv.config();
 
-const mongoClient = new MongoClient("mongodb://localhost:27017");
 let db;
-mongoClient.connect().then(() => {
+
+const mongoClient = new MongoClient(process.env.MONGO_URL);
+await mongoClient.connect().then( () => {
     db = mongoClient.db("db-batepapo-uol");
-})
+}).catch( e => console.log("Database conection problem", e));
+
+const participants = db.collection("participants");
+const mensages = db.collection("mensages");
 
 let user;
-let participants = [];
-let mensages = [];
 
-// app.post("/participants", async (req, res) => {
-//     try{
-//         wait 
-//     }
-// });
+app.post("/participants", async (req, res) => {
+
+    let userSchema = joi.object({
+        name: joi.string().required()
+    });
+    let validation = userSchema.validate(req.body.name);
+    if (!validation){
+        res.sendStatus(422)
+    }
+    try{
+        await participants.insertOne({
+            name: req.body.name
+        }).then(() => console.log(participants))
+    }catch(e){console.log(e)}
+});
 
 // app.get("/participants");
 
