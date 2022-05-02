f7ba1b88632ded3515709e165d772997f617f15a
ManuelHMR <manuelhmrjf@gmail.com>
2022-05-02 13:22:10 -0300

    headers validation

--- app.js
+++ app.js
@@ -21,9 +21,9 @@ const messages = db.collection("messages");
 
 let userSchema = joi.string().required();
 let messageSchema = joi.object({
-    to: joi.string().min(1).required(),
-    text: joi.string().min(1).required(),
-    type: joi.string().valid("message", "private_message").required()
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.valid("message", "private_message").required()
 });
 
 app.post("/participants", async (req, res) => {
@@ -68,13 +68,13 @@ app.get("/participants", async (req, res) => {
 app.post("/messages", async (req, res) => {
     const {to, text, type} = req.body;
     const from = req.headers.user;
-    const validation = messageSchema.validate(req.body);
-    //headersCheck
+    const bodyValidation = messageSchema.validate(req.body);
+    const headersValidation = await participants.findOne({name: req.headers.user});
     try{
-        if(validation.error){
+        if(bodyValidation.error || !headersValidation){
             res.sendStatus(422);
         };
-        if(!validation.error){
+        if(!bodyValidation.error && headersValidation){
             const message = {
                 from,
                 to,
@@ -101,9 +101,14 @@ app.get("/messages", async (req, res) => {
 
 app.post("/status", async (req, res) => {
     try{
-
-        await participants.updateOne({name: req.headers.user}, {$set: {lastStatus: Date.now()}});
-        res.sendStatus(200);
+        const validation = await participants.findOne({name: req.headers.user});
+        if(!validation){
+            res.sendStatus(404);
+        };
+        if(validation){
+            await participants.updateOne({name: req.headers.user}, {$set: {lastStatus: Date.now()}});
+            res.sendStatus(200);
+        }
     } catch (err) {
         res.send(err);
     }
